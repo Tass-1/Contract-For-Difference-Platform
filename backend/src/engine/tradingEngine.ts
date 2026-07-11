98b9da7b4604e108101c5c315d3588d60fcd4d95
Tass-1 <priyanshujoshi10000@gmail.com>
2026-07-11 19:08:03 +0530

    app missing fix

--- backend/src/engine/tradingEngine.ts
+++ backend/src/engine/tradingEngine.ts
@@ -6,6 +6,7 @@ import {Server} from "socket.io";
 import express from "express";
 import http from "http";
 
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
