f29394c9b7cca80ab8dfe5b3f8ce1a6486992b57
Tass-1 <priyanshujoshi10000@gmail.com>
2026-07-11 21:04:13 +0530

    fix for redis worker + optimization

--- backend/src/jobs/worker.ts
+++ backend/src/jobs/worker.ts
@@ -1,33 +1,42 @@
 
 import {Worker} from "bullmq";
-import {connection} from "./queue.ts";
-import redis  from "../config/Redis.ts";
-import schema from "../models/schema.ts";
-import {UserModel} from "../models/schema.ts";
+import {connection} from "./queue";
+import redis  from "../config/Redis";
+import schema from "../models/schema";
+import {UserModel} from "../models/schema";
 import mongoose from "mongoose";
-import {OrderModel} from "../models/schema.ts";
+import {OrderModel} from "../models/schema";
 
 const worker = new Worker(
     "limitOrders",
     async (job) => {
         console.log("looking for jobs to remove")
         if(job.name == "remove-expired-orders"){
-            const keys = await redis.keys("limitOrder:*");
-            for( const key of keys){
-                const orders = await redis.hgetall(key);
+            try{
+                const keys = await redis.keys("limitOrder:*");
+                for( const key of keys){
+                    const orders = await redis.hgetall(key);
 
-                for ( const orderId in orders){
-                    const order = JSON.parse(orders[orderId]);
+                    for ( const orderId in orders){
+                        const order = JSON.parse(orders[orderId]);
 
-                    if(new Date(order.expiry) < new Date()){
-                        const userId = new mongoose.Types.ObjectId(order.userId)
-                        const OrderId = new mongoose.Types.ObjectId(order.orderId)
-                        await UserModel.updateOne({ _id: userId} , {$inc: {balance: order.margin}});
-                        await OrderModel.updateOne({ _id: OrderId} , {status:"expired"});
-                        await redis.hdel(key,orderId)
+                        if(new Date(order.expiry) < new Date()){
+                            const userId = new mongoose.Types.ObjectId(order.userId)
+                            const OrderId = new mongoose.Types.ObjectId(order.orderId)
+                            await Promise.all([
+                                UserModel.updateOne({ _id: userId} , {$inc: {balance: order.margin}}),
+                                OrderModel.updateOne({ _id: OrderId} , {status:"expired"}),
+                                redis.hdel(key,orderId)
+                            ])
+                        }
                     }
                 }
             }
+            catch(err){
+                console.error("Worker process error" , err)
+                throw err
+            }
+            
         }
     },
     {connection}
